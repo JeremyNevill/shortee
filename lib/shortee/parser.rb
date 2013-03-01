e83df635c1f7a11d0bfcddd73b28fc049f2e18df
Jeremy Nevill <jeremy@nevill.net>
2013-03-01 16:10:08 +0000

    Refactor and rename of shortee to short

--- lib/shortee/parser.rb
+++ lib/shortee/parser.rb
@@ -4,17 +4,13 @@ module Shortee
 
   class Shortee::Parser < Parslet::Parser
 
-    # A space and many spaces
+    # Generic rules
     rule(:space) { match('\s').repeat(1) }
     rule(:space?) { space.maybe }
-
-    # Other rules
     rule(:dash?) { match['_-'].maybe }
     rule(:at) { str('@') | (dash? >> (str('at') | str('AT')) >> dash?) }
     rule(:dot) { str('.') | (dash? >> (str('dot') | str('DOT')) >> dash?) }
-
     rule(:forwardslash) { match['/'] }
-
     rule(:integer) { match('[0-9]').repeat(1)>> space? }
     rule(:word) { match('[a-zA-Z0-9]').repeat(1).as(:word) >> space? }
     rule(:separator) { dot.as(:dot) >> space? | space }
@@ -24,49 +20,32 @@ module Shortee
     rule(:actor) { match('[a-zA-Z0-9]').repeat(1).as(:actor) >> space? }
     rule(:actee) { match('[a-zA-Z0-9]').repeat(1).as(:actee) >> space? }
     rule(:action) { match('[a-zA-Z]').repeat(1).as(:action) >> space? }
-    rule(:amountnum) { match('[0-9.]').repeat(1).as(:amountnum) }
+    rule(:amountnum) { match('[0-9.]').repeat(1).as(:amountnum) >> space? }
     rule(:amountunits) { match('[a-zA-Z]').repeat(1).as(:amountunits) >> space? }
-
     rule(:day) { match('[0-9]').repeat(1).as(:day) }
-    rule(:month) { match('[a-zA-Z]').repeat(1).as(:month) }
+    rule(:month) { match('[a-zA-Z0-9]').repeat(1).as(:month) }
     rule(:year) { match('[0-9]').repeat(1).as(:year) }
-    rule(:ukmonth) { match('[0-9]').repeat(1).as(:ukmonth) }
 
-    rule(:shorteeshort) {
+    rule(:date) {
+      (day >> forwardslash >> month >> forwardslash >> year)
+    }
+
+    rule(:short) {
       (space? >> at >> actor.as(:mainactor) >>
           action >>
           amountnum >> amountunits >>
-          day >> forwardslash >>
-          month >> forwardslash >> year).as(:shortee)
+          date).as(:short)
     }
 
-    rule(:shorteeukshort) {
-      (space? >> at >> actor.as(:mainactor) >>
-          action >>
-          amountnum >> amountunits >>
-          day >> forwardslash >>
-          ukmonth >> forwardslash >> year).as(:shortee)
-    }
-
-    rule(:shorteelong) {
+    rule(:shortwithactee) {
       (space? >> at >> actor.as(:mainactor) >>
           action >>
           at >> actee.as(:actee) >>
           amountnum >> amountunits >>
-          day >> forwardslash >>
-          month >> forwardslash >> year).as(:shortee)
+          date).as(:short)
     }
 
-    rule(:shorteeuklong) {
-      (space? >> at >> actor.as(:mainactor) >>
-          action >>
-          at >> actee.as(:actee) >>
-          amountnum >> amountunits >>
-          day >> forwardslash >>
-          ukmonth >> forwardslash >> year).as(:shortee)
-    }
-
-    rule(:allshorts) { shorteeshort | shorteeukshort | shorteelong | shorteeuklong }
+    rule(:allshorts) { short | shortwithactee }
 
     root(:allshorts)
   end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -4,73 +4,87 @@ require 'shortee'
 
 describe Shortee::Parser do
 
-  it "should parse an actee" do
+  it "parses a mainactor" do
+    test_message="@somebody did 1thing 01/jan/2012"
+    parser = Shortee::Parser.new
+    parsed_message = parser.parse(test_message)
+    parsed_message[:short][:mainactor][:actor].to_s.should eq("somebody")
+  end
+
+  it "parses an actee" do
     test_message="@actor didto @actee 1something 01/12/2012"
     parser = Shortee::Parser.new
     parsed_message = parser.parse(test_message)
-    parsed_message[:shortee][:actee][:actee].to_s.should eq("actee")
+    parsed_message[:short][:actee][:actee].to_s.should eq("actee")
   end
 
-  it "should parse another actee" do
+  it "parses another actee" do
     test_message="@actor didto @jimboWilliams 1something 01/jan/2012"
     parser = Shortee::Parser.new
     parsed_message = parser.parse(test_message)
-    parsed_message[:shortee][:actee][:actee].to_s.should eq("jimboWilliams")
+    parsed_message[:short][:actee][:actee].to_s.should eq("jimboWilliams")
   end
 
-  it "should parse a valid actee" do
-    test_message="@actor gave @frank 2cheeses 01/jan/2012"
-    parser = Shortee::Parser.new
-    parsed_message = parser.parse(test_message)
-    parsed_message[:shortee][:actee][:actee].to_s.should eq("frank")
-  end
-
-  it "should parse a valid mainactor" do
+  it "parses an action" do
     test_message="@somebody did 1thing 01/jan/2012"
     parser = Shortee::Parser.new
     parsed_message = parser.parse(test_message)
-    parsed_message[:shortee][:mainactor][:actor].to_s.should eq("somebody")
+    parsed_message[:short][:action].to_s.should eq("did")
   end
 
-  it "should parse a valid action" do
+  it "parses an amount" do
     test_message="@somebody did 1thing 01/jan/2012"
     parser = Shortee::Parser.new
     parsed_message = parser.parse(test_message)
-    parsed_message[:shortee][:action].to_s.should eq("did")
+    parsed_message[:short][:amountnum].should eq("1")
   end
 
-  it "should parse a valid amount" do
+  it "parses a 1 decimal place amount" do
+    test_message="@somebody did 0.2things 01/jan/2012"
+    parser = Shortee::Parser.new
+    parsed_message = parser.parse(test_message)
+    parsed_message[:short][:amountnum].should eq("0.2")
+  end
+
+  it "parses a unit" do
+    test_message="@somebody did 1jibberjabber 01/jan/2012"
+    parser = Shortee::Parser.new
+    parsed_message = parser.parse(test_message)
+    parsed_message[:short][:amountunits].should eq("jibberjabber")
+  end
+
+  it "parses a unit with a space before it" do
+    test_message="@somebody did 1 jibberjabber 01/jan/2012"
+    parser = Shortee::Parser.new
+    parsed_message = parser.parse(test_message)
+    parsed_message[:short][:amountunits].should eq("jibberjabber")
+  end
+
+  it "parses a full standard uk date dd/mmm/yyyy" do
     test_message="@somebody did 1thing 01/jan/2012"
     parser = Shortee::Parser.new
     parsed_message = parser.parse(test_message)
-    parsed_message[:shortee][:amountnum].should eq("1")
+    parsed_message[:short][:day].should eq("01")
+    parsed_message[:short][:month].should eq("jan")
+    parsed_message[:short][:year].should eq("2012")
   end
 
-  it "should parse a valid full date" do
-    test_message="@somebody did 1thing 01/jan/2012"
-    parser = Shortee::Parser.new
-    parsed_message = parser.parse(test_message)
-    parsed_message[:shortee][:day].should eq("01")
-    parsed_message[:shortee][:month].should eq("jan")
-    parsed_message[:shortee][:year].should eq("2012")
-  end
-
-  it "should parse a valid UK date" do
+  it "parses a UK numeric date" do
     test_message="@somebody did 1thing 01/12/2012"
     parser = Shortee::Parser.new
     parsed_message = parser.parse(test_message)
-    parsed_message[:shortee][:day].should eq("01")
-    parsed_message[:shortee][:ukmonth].should eq("12")
-    parsed_message[:shortee][:year].should eq("2012")
+    parsed_message[:short][:day].should eq("01")
+    parsed_message[:short][:month].should eq("12")
+    parsed_message[:short][:year].should eq("2012")
   end
 
-  it "should parse another valid UK date" do
+  it "parses another UK numeric date" do
     test_message="@frankee ate 2cheeses 13/01/2012"
     parser = Shortee::Parser.new
     parsed_message = parser.parse(test_message)
-    parsed_message[:shortee][:day].should eq("13")
-    parsed_message[:shortee][:ukmonth].should eq("01")
-    parsed_message[:shortee][:year].should eq("2012")
+    parsed_message[:short][:day].should eq("13")
+    parsed_message[:short][:month].should eq("01")
+    parsed_message[:short][:year].should eq("2012")
   end
 
 end
